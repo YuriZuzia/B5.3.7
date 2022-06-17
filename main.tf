1583ef58c378c74c8b6a2f191e86b5be7b4c83fe
YuriZuzia <teanjam@ya.ru>
2022-06-17 13:09:36 +0500

    add comment

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ terraform {
   required_version = ">= 0.13"
 }
 provider "yandex" {
+  //OAth-token described in separate secure.tf file
   token = "${var.OAuth-token}"
   cloud_id = "${var.cloud_id}"
   folder_id = "${var.folder_id}"
